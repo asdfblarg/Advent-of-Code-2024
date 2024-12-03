317be18cdf347bf31accf3c637e5d49f59a56bd2
asdfblarg <david82794@yahoo.com>
2024-12-02 21:27:17 -0500

    run black

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,28 +2,35 @@ import pandas as pd
 
 # Part 1
 
+
 def check_all_increasing_or_decreasing(level):
     all_increasing = True
     all_decreasing = True
     for i in range(1, len(level)):
-        if level[i-1] > level[i]:
+        if level[i - 1] > level[i]:
             all_increasing = False
-            
-        if level[i-1] < level[i]:
+
+        if level[i - 1] < level[i]:
             all_decreasing = False
 
     return all_increasing or all_decreasing
 
+
 def check_diff(level):
     for i in range(1, len(level)):
-        if not 1 <= abs(level[i-1] - level[i]) <= 3:
+        if not 1 <= abs(level[i - 1] - level[i]) <= 3:
             return False
     return True
 
-input_df = pd.read_csv("input.txt", names=["levels"])
-input_df["levels"] = input_df["levels"].str.split(" ").apply(lambda x: [int(num) for num in x])
 
-input_df["all_incr_decre"] = input_df["levels"].apply(check_all_increasing_or_decreasing)
+input_df = pd.read_csv("input.txt", names=["levels"])
+input_df["levels"] = (
+    input_df["levels"].str.split(" ").apply(lambda x: [int(num) for num in x])
+)
+
+input_df["all_incr_decre"] = input_df["levels"].apply(
+    check_all_increasing_or_decreasing
+)
 input_df["diff_ok"] = input_df["levels"].apply(check_diff)
 input_df["level_safe"] = input_df["all_incr_decre"] & input_df["diff_ok"]
 
@@ -32,11 +39,12 @@ print("Part 1:", part_1_ans)
 
 # Part 2
 
+
 def check_diff_damper(level):
     for i in range(len(level)):
-#        new_level = level.copy()
-#        new_level.pop(i)
-        new_level = level[:i] + level[i+1:]
+        #        new_level = level.copy()
+        #        new_level.pop(i)
+        new_level = level[:i] + level[i + 1 :]
         all_incr_decre = check_all_increasing_or_decreasing(new_level)
         diff_ok = check_diff(new_level)
 
@@ -44,6 +52,7 @@ def check_diff_damper(level):
             return True
 
     return False
+
 
 input_df["level_damper_safe"] = input_df["levels"].apply(check_diff_damper)
 

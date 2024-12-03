4d8063675fcb65f93b007094c880d2444579e186
asdfblarg <david82794@yahoo.com>
2024-12-02 21:11:17 -0500

    add cleaned up .py for day 1

---
+++ day_1/day_1.py
@@ -0,0 +1,29 @@
+from collections import Counter
+import pandas as pd
+
+# Part 1
+
+input_df = pd.read_csv("input.txt", sep="\s+", names=["list_1", "list_2"])
+
+list_1 = sorted(input_df["list_1"].to_list())
+list_2 = sorted(input_df["list_2"].to_list())
+
+diff = []
+for i in range(len(list_1)):
+    diff.append(abs(list_1[i] - list_2[i]))
+
+part_1_ans = sum(diff)
+print("Part 1:", part_1_ans)
+
+# Part 2
+
+list_1_dict = Counter(list_1)
+list_2_dict = Counter(list_2)
+
+similarities = []
+for i in range(len(list_1)):
+    list_id = list_1[i]
+    similarities.append(list_id * list_2_dict[list_id])
+
+part_2_ans = sum(similarities)
+print("Part 2:", part_2_ans)

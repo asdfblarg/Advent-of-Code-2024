10f59296e9d4da73c3f510864029b9f033d95b9d
asdfblarg <david82794@yahoo.com>
2024-12-03 11:25:16 -0500

    add cleaned up day_3.py

---
+++ day_3/day_3.py
@@ -0,0 +1,25 @@
+import re
+
+with open("input.txt") as f:
+    lines = f.readlines()
+    joined_lines = "".join(lines)
+
+
+def calc_line_mul_sum(line):
+    line_mul = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", line)
+
+    line_res = []
+    for mul in line_mul:
+        matches = re.search(r"mul\(([0-9]+),([0-9]+)\)", mul)
+        line_res.append(int(matches[1]) * int(matches[2]))
+
+    return sum(line_res)
+
+
+part_1_ans = calc_line_mul_sum(joined_lines)
+print("Part 1:", part_1_ans)
+
+cleaned_donts = re.sub(r"don't\(\).+?do\(\)", "", joined_lines)
+part_2_ans = calc_line_mul_sum(cleaned_donts)
+
+print("Part 2:", part_2_ans)

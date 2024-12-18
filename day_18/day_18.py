232c455e5679d64cddd4bde035c674214a87147a
asdfblarg <david82794@yahoo.com>
2024-12-18 00:47:37 -0500

    add day_18.py

---
+++ day_18/day_18.py
@@ -0,0 +1,56 @@
+from collections import deque
+
+with open("input.txt") as f:
+    lines = f.read().split("\n")
+
+coords = [line.split(",") for line in lines]
+coords = [(int(coord[1]), int(coord[0])) for coord in coords]
+
+
+rows = cols = 70 + 1
+grid = [["." for i in range(rows)] for j in range(cols)]
+
+
+def bfs_shortest_path(grid, rows, cols):
+    queue = deque()
+    queue.append((0, 0, 0))
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    visited = {}
+    while queue:
+        x, y, steps = queue.popleft()
+        for direction in directions:
+            new_x, new_y = x + direction[0], y + direction[1]
+            new_steps = steps + 1
+            if 0 <= new_x < rows and 0 <= new_y < rows and grid[new_x][new_y] != "#":
+                if (new_x, new_y) not in visited:
+                    visited[(new_x, new_y)] = new_steps
+                    queue.append((new_x, new_y, new_steps))
+
+    return visited.get((rows - 1, cols - 1), -1)
+
+
+mem_space = grid.copy()
+for coord in coords[:1024]:
+    x, y = coord
+    mem_space[x][y] = "#"
+
+shortest_path = bfs_shortest_path(mem_space, rows, cols)
+print("Part 1:", shortest_path)
+
+# Part 2
+#
+
+mem_space = [["." for i in range(rows)] for j in range(cols)]
+count = 1
+for coord in coords:
+    print(f"{count} out of {len(coords)}", end="\r")
+    x, y = coord
+    mem_space[x][y] = "#"
+    shortest_path = bfs_shortest_path(mem_space, rows, cols)
+    if shortest_path == -1:
+        break
+
+    count += 1
+
+print("Part 2:", f"{y},{x}")
